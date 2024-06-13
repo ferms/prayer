80d0f3aade59afb0bb26383f1d2d67f0258172f3
Fernando R Montt <fernando.montano@sinapsis.global>
2024-06-13 14:38:30 -0500

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SQA5PSCZ.mjs";import"./chunk-DDCYFZHC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-APU2JOY4.mjs";import"./chunk-DDCYFZHC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
