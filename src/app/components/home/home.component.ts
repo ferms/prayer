b66b0201dd518644f468ab69dfac3cf39ece3210
Fernando R Montt <fernando.montano@sinapsis.global>
2024-06-16 12:53:55 -0500

    img and sections

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RosarioComponent } from "../rosario/rosario.component";
 import { AccordionModule } from 'primeng/accordion';
 import { CardModule } from 'primeng/card';
+import { ViacrucisComponent } from "../viacrucis/viacrucis.component";
+import { OrgSntBrigidaOneComponent } from "../org-snt-brigida-one/org-snt-brigida-one.component";
+import { OrgConsagradoComponent } from "../org-consagrado/org-consagrado.component";
+import { OrgLatinComponent } from "../org-latin/org-latin.component";
+import { OrgOtrasComponent } from "../org-otras/org-otras.component";
+import { OrgSntBrigidaTwelveComponent } from "../org-snt-brigida-twelve/org-snt-brigida-twelve.component";
+import { DoloresMariaComponent } from "../dolores-maria/dolores-maria.component";
+import { DomingosSanjoComponent } from "../domingos-sanjo/domingos-sanjo.component";
+import { CoronillaMisericordiaComponent } from "../coronilla-misericordia/coronilla-misericordia.component";
+import { CoronillaSangreCristoComponent } from "../coronilla-sangre-cristo/coronilla-sangre-cristo.component";
 
 
 @Component({
@@ -9,7 +19,21 @@ import { CardModule } from 'primeng/card';
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [AccordionModule,CardModule, RosarioComponent]
+    encapsulation: ViewEncapsulation.None,
+    imports: [
+        AccordionModule,
+        CardModule,
+        RosarioComponent,
+        ViacrucisComponent,
+        OrgSntBrigidaOneComponent,
+        OrgConsagradoComponent,
+        OrgLatinComponent,
+        OrgOtrasComponent,
+        OrgSntBrigidaTwelveComponent,
+        DoloresMariaComponent,
+        DomingosSanjoComponent,
+        CoronillaMisericordiaComponent,
+        CoronillaSangreCristoComponent]
 })
 export class HomeComponent {
 
--- src/app/components/rosario/rosario.component.ts
+++ src/app/components/rosario/rosario.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FieldsetModule } from 'primeng/fieldset';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-rosario',
   standalone: true,
-  imports: [],
+  encapsulation: ViewEncapsulation.None,
+  imports: [FieldsetModule, ImageModule],
   templateUrl: './rosario.component.html',
   styleUrl: './rosario.component.scss'
 })
